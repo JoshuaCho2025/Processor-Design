4169a3c0a94651e7a3cf28ae0979c8d835542512
JoshuaCho2025 <jsc9820@nyu.edu>
2024-04-18 14:57:37 -0400

    Changed Name of Lab1_CMake

---
+++ Lab1_CMake/build/CMakeFiles/hello2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello2.dir/src/hello.cpp.o"
+  "CMakeFiles/hello2.dir/src/hello.cpp.o.d"
+  "CMakeFiles/hello2.dir/src/name.cpp.o"
+  "CMakeFiles/hello2.dir/src/name.cpp.o.d"
+  "hello2"
+  "hello2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
